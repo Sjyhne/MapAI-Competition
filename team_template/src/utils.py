f235a51461212977ca29c05c306a074a910aaa3b
LM239 <larsmartin@hodne.net>
2022-10-21 14:40:30 +0200

    create runnable base framework

--- team_template/src/utils.py
+++ team_template/src/utils.py
@@ -1,6 +1,7 @@
 import os
 import glob
 import torch
+import segmentation_models_pytorch as smp
 
 def create_run_dir(opts):
 
@@ -29,8 +30,6 @@ def create_run_dir(opts):
 
     return runpath
 
-
-
 def store_model_weights(opts: dict, model: torch.nn.Module, type: str, epoch: int):
     rundir = opts["rundir"]
     files = glob.glob(os.path.join(rundir, f"{type}_*.pt"))
@@ -43,3 +42,79 @@ def record_scores(opts, scoredict):
 
     with open(os.path.join(rundir, "run.log"), "a") as f:
         f.write(str(scoredict) + "\n")
+
+def get_optimizer(opts, model):
+    optimizer_cfg = opts["training"]["optimizer"]
+    if optimizer_cfg["name"] == "Adam":
+        return torch.optim.Adam(model.parameters(), lr=optimizer_cfg["lr"])
+    elif optimizer_cfg["name"] == "SGD":
+        return torch.optim.SGD(model.parameters(), lr=optimizer_cfg["lr"])
+    else:
+        print(f"Optimizer {optimizer_cfg['name']} is not implemented")
+        exit()
+
+# TODO: test and use
+def get_losses(opts):
+    losses_cfg = opts["training"]["losses"]
+
+    losses = {
+        "DiceLoss": smp.losses.DiceLoss,
+        "JaccardLoss": smp.losses.JaccardLoss,
+        "TverskyLoss": smp.losses.TverskyLoss,
+        "FocalLoss": smp.losses.FocalLoss,
+        "LovaszLoss": smp.losses.LovaszLoss,
+        "SoftBCEWithLogitsLoss": smp.losses.SoftBCEWithLogitsLoss,
+        "SoftCrossEntropyLoss": smp.losses.SoftCrossEntropyLoss
+    }
+    used_losses = []
+    weights = torch.tensor(losses_cfg["weights"])
+    for loss_name in losses_cfg:
+        used_losses.append(losses[loss_name](**losses_cfg[loss_name]))
+
+    def multiloss(preds, targets):
+        loss = 0
+        for i in range(len(used_losses)):
+            loss += weights[i] * used_losses[i](preds, targets)
+        return loss
+
+    return multiloss
+
+def get_model(opts):
+    model_cfg = opts["model"]
+    model = None
+    if model_cfg["name"] == "Unet":
+        model = smp.Unet(
+            encoder_name=model_cfg.get("encoder", "resnet34"),        
+            encoder_weights=model_cfg.get("encoder_weights", "imagenet"),    
+            in_channels=model_cfg.get("in_channels", 3),
+            classes=opts.get("num_classes", 3),
+            encoder_depth=model_cfg.get("encoder_depth", 5)
+        )
+    elif model_cfg["name"] == "UNet++":
+        model = smp.UnetPlusPlus(
+            encoder_name=model_cfg.get("encoder", "resnet34"),        
+            encoder_weights=model_cfg.get("encoder_weights", "imagenet"),    
+            in_channels=model_cfg.get("in_channels", 3),
+            classes=opts.get("num_classes", 3),
+            encoder_depth=model_cfg.get("encoder_depth", 5)
+        )
+    elif model_cfg["name"] == "FPN":
+        model = smp.FPN(
+            encoder_name=model_cfg.get("encoder", "resnet34"),        
+            encoder_weights=model_cfg.get("encoder_weights", "imagenet"),    
+            in_channels=model_cfg.get("in_channels", 3),
+            classes=opts.get("num_classes", 3),
+            encoder_depth=model_cfg.get("encoder_depth", 5)
+        )
+    elif model_cfg["name"] == "DeepLabV3+":
+        model = smp.DeepLabV3Plus(
+            encoder_name=model_cfg.get("encoder", "resnet34"),        
+            encoder_weights=model_cfg.get("encoder_weights", "imagenet"),    
+            in_channels=model_cfg.get("in_channels", 3),
+            classes=opts.get("num_classes", 3),
+            encoder_depth=model_cfg.get("encoder_depth", 5)
+        )
+    else:
+        print(f"Model {model_cfg['name']} is not available")
+        exit()
+    return model
