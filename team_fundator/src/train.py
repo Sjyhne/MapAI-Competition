744584b5919d3fe514fe7e46c8f33a0c17852a2b
LM239 <larsmartin@hodne.net>
2022-10-27 10:33:53 +0200

    add auxhead and multiprocessing, change task 3

--- team_fundator/src/train.py
+++ team_fundator/src/train.py
@@ -17,37 +17,47 @@ from competition_toolkit.eval_functions import calculate_score
 import transforms
 
 transforms = transforms.__dict__
-print(transforms.keys())
-def test(dataloader, model, lossfn, device, interpolation_mode=torchvision.transforms.InterpolationMode.BILINEAR, antialias=True):
+def test(dataloader, model, lossfn, device, aux_loss=None, interpolation_mode=torchvision.transforms.InterpolationMode.BILINEAR, antialias=True, aux_head=False):
     model.eval()
 
     device = device
 
 
     losstotal = np.zeros((len(dataloader)), dtype=float)
+    lossesseg = np.zeros((len(dataloader)), dtype=float)
+    lossesaux = np.zeros((len(dataloader)), dtype=float)
     ioutotal = np.zeros((len(dataloader)), dtype=float)
     bioutotal = np.zeros((len(dataloader)), dtype=float)
     scoretotal = np.zeros((len(dataloader)), dtype=float)
 
     for idx, batch in tqdm(enumerate(dataloader), leave=False, total=len(dataloader), desc="Test"):
-        image, label = batch.values()
+        if aux_head:
+            image, label, aux_label = batch.values() 
+            aux_label = aux_label.to(device)
+        else:
+            image, label = batch.values()
         image = image.to(device)
-        label = label.long().to(device) if opts["task"] != 3 else label.to(device)
+        label = label.long().to(device)
         
-
+        loss_aux = 0
         output = model(image)
-        if opts["task"] == 3:
-            output = output.squeeze(dim=1)
+        if aux_head:
+            output, aux_pred = output
+            loss_aux = aux_loss(aux_pred, aux_label).item()
+            lossesaux[idx] = loss_aux
+
         if label.shape[-2:] != output.shape[-2:]:
             output = torchvision.transforms.functional.resize(output, (500, 500), interpolation=interpolation_mode, antialias=antialias)
+        losseg = lossfn(output, label).item()
+        lossesseg[idx] = losseg
+        losstotal[idx] = loss_aux + losseg
 
-        loss = lossfn(output, label).item()
-        losstotal[idx] = loss
+        if output.shape[1] > 1:
+            output = torch.argmax(torch.softmax(output, dim=1), dim=1)
+        else:
+            output = torch.round(torch.sigmoid(output)).squeeze(1)
+        label = label.squeeze(1)
 
-        if opts["task"] == 3:
-            continue
-
-        output = torch.argmax(torch.softmax(output, dim=1), dim=1)
         if device != "cpu":
             metrics = calculate_score(output.detach().cpu().numpy().astype(np.uint8),
                                     label.detach().cpu().numpy().astype(np.uint8))
@@ -61,9 +71,11 @@ def test(dataloader, model, lossfn, device, interpolation_mode=torchvision.trans
 
     iou = round(ioutotal[0].mean(), 4)
     loss = round(losstotal[0].mean(), 4)
+    lossesseg = round(lossesseg[0].mean(), 4)
+    lossesaux = round(lossesaux[0].mean(), 4)
     biou = round(bioutotal[0].mean(), 4)
     score = round(scoretotal[0].mean(), 4)
-    return loss, iou, biou, score
+    return loss, lossesaux, lossesseg, iou, biou, score
 
 
 def train(opts):
@@ -72,32 +84,33 @@ def train(opts):
     # The current model should be swapped with a different one of your choice
 
     model = get_model(opts)
+    if opts["weights"] is not None:
+        model.load_state_dict(torch.load(opts["weights"]))
 
     model.to(device)
     model = model.float()
+    aux_head = opts["model"]["aux_head"]
 
     optimizer = get_optimizer(opts, model)
     scheduler = get_scheduler(opts, optimizer)
     lossfn = get_losses(opts)
+    aux_loss = torch.nn.BCEWithLogitsLoss()
 
     epochs = opts["train"]["epochs"]
 
 
     augmentation_cfg = opts["augmentation"]
-    initial_transform = transforms[augmentation_cfg.get("initial", "normal")]
+    initial_transform = transforms[augmentation_cfg.get("initial", "normal")](opts["imagesize"])
     aug_list = get_aug_names(opts, augmentation_cfg, transforms)
 
-
-
     lidar_transform = None
-    if opts["task"] >= 2:
+    if opts["task"] == 2:
         lidar_transform = get_lidar_transform(opts)
 
-    trainloader = create_dataloader(opts, "train", transforms=(initial_transform, lidar_transform))
-    valloader = create_dataloader(opts, "validation", transforms=(valid_transform, lidar_transform))
+    trainloader = create_dataloader(opts, "train", transforms=(initial_transform, lidar_transform), aux_head_labels=aux_head)
+    valloader = create_dataloader(opts, "validation", transforms=(valid_transform, lidar_transform), aux_head_labels=aux_head)
 
     bestscore = 0
-
 
     print("Training with augmentation schedule: ", aug_list)
 
@@ -107,6 +120,8 @@ def train(opts):
         model.train()
 
         losstotal = np.zeros((len(trainloader)), dtype=float)
+        losses_aux = np.zeros((len(trainloader)), dtype=float)
+        losses_seg = np.zeros((len(trainloader)), dtype=float)
         scoretotal = np.zeros((len(trainloader)), dtype=float)
         ioutotal = np.zeros((len(trainloader)), dtype=float)
         bioutotal = np.zeros((len(trainloader)), dtype=float)
@@ -116,19 +131,47 @@ def train(opts):
         if e >= augmentation_cfg["warmup_epochs"]:
             aug = aug_list[e]
             print(f"Using the {aug} transform")
-            new_transform = transforms[aug]
+            new_transform = transforms[aug](opts["imagesize"])
             trainloader.dataset.set_transform(new_transform)
 
         for idx, batch in tqdm(enumerate(trainloader), leave=True, total=len(trainloader), desc="Train", position=0):
-            image, label = batch.values()
+            if aux_head:
+                image, label, aux_label = batch.values() 
+                aux_label = aux_label.to(device)
+            else:
+                image, label = batch.values()
             image = image.to(device)
-            label = label.long().to(device) if opts["task"] != 3 else label.to(device)
+            label = label.long().to(device) 
 
 
+            loss_aux = 0
             output = model(image)
+            
             if opts["task"] == 3:
-                output = output.squeeze(dim=1)
-            loss = lossfn(output, label)
+                pass
+                # output = output.squeeze(dim=1)
+                # img = output.squeeze().detach().cpu().numpy().copy()
+                # img = np.exp(img) + math.e
+                # maximum = np.max(img)
+                # img = img * (255.0/maximum)
+
+                # img = np.stack([img, img, img], axis=-1).astype(np.uint8)
+                # print(img.shape)
+                # cv2.imwrite(opts["rundir"] + f"/pred{idx}.png", img)
+
+                # pred_img = image.squeeze().cpu().numpy() * 255.0
+                # cv2.imwrite(opts["rundir"] + f"/pred{idx}img.png", pred_img.transpose(1, 2, 0).astype(np.uint8))
+                # if idx == 20:
+                #     exit()
+            elif aux_head:
+                output, aux_pred = output
+                loss_aux = aux_loss(aux_pred, aux_label)
+                losses_aux[idx] = loss_aux.item()
+
+            loss_seg = lossfn(output, label)
+            losses_seg[idx] = loss_seg.item()
+
+            loss = loss_aux + loss_seg
 
             optimizer.zero_grad()
             loss.backward()
@@ -136,10 +179,12 @@ def train(opts):
 
             lossitem = loss.item()
             losstotal[idx] = lossitem
-            if opts["task"] == 3:
-                continue
+            if output.shape[1] > 1:
+                output = torch.argmax(torch.softmax(output, dim=1), dim=1)
+            else:
+                output = torch.round(torch.sigmoid(output)).squeeze(1)
+            label = label.squeeze(1)
 
-            output = torch.argmax(torch.softmax(output, dim=1), dim=1)
             if device != "cpu":
                 trainmetrics = calculate_score(output.detach().cpu().numpy().astype(np.uint8),
                                                label.detach().cpu().numpy().astype(np.uint8))
@@ -152,8 +197,10 @@ def train(opts):
             scoretotal[idx] = trainmetrics["score"]
         scheduler.step()
 
-        testloss, testiou, testbiou, testscore = test(valloader, model, lossfn, device)
+        testloss, testlossaux, testlosseg, testiou, testbiou, testscore = test(valloader, model, lossfn, device, aux_loss=aux_loss, aux_head=aux_head)
         trainloss = round(losstotal.mean(), 4)
+        trainlosseg = round(losses_seg.mean(), 4)
+        trainlossaux = round(losses_aux.mean(), 4)
         trainiou = round(ioutotal.mean(), 4)
         trainbiou = round(bioutotal.mean(), 4)
         trainscore = round(scoretotal.mean(), 4)
@@ -167,8 +214,8 @@ def train(opts):
         print("New learning rate", scheduler.get_lr())
         print("")
         print(tabulate(
-            [["train", trainloss, trainiou, trainbiou, trainscore], ["test", testloss, testiou, testbiou, testscore]],
-            headers=["Type", "Loss", "IoU", "BIoU", "Score"]))
+            [["train", trainloss, trainlossaux, trainlosseg, trainiou, trainbiou, trainscore], ["test", testloss, testlossaux, testlosseg, testiou, testbiou, testscore]],
+            headers=["Type", "LossTotal", "LossAux", "LossSeg", "IoU", "BIoU", "Score"]))
 
         scoredict = {
             "epoch": e,
@@ -192,6 +239,7 @@ if __name__ == "__main__":
     parser.add_argument("--epochs", type=int, default=10, help="Number of epochs for training")
     parser.add_argument("--config", type=str, default="config/data.yaml", help="Configuration file to be used")
     parser.add_argument("--task", type=int, default=1)
+    parser.add_argument("--weights", type=str, default=None)
 
     args = parser.parse_args()
 
@@ -205,6 +253,8 @@ if __name__ == "__main__":
         opts = {**opts, **vars(args)}
 
     print("Opts:", opts)
+    if opts["use_lidar_as_mask"]:
+        opts["num_classes"] = 3
 
     rundir = create_run_dir(opts, opts.get("dataset", ""))
     opts["rundir"] = rundir
