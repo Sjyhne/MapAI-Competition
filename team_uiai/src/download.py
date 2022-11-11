f7e047946ef77cb4afb6c5e95aa92cbe8d8dc44b
Pedro Alves <pmcalves1@gmail.com>
2022-11-11 14:14:53 +0100

    added download file

---
+++ team_uiai/src/download.py
@@ -0,0 +1,9 @@
+from competition_toolkit.dataloader import download_dataset
+data_type = "train" # or "validation"
+task = 1 # or 2, but does not matter for train and validation data types
+# paths is a list of dicts, where each dict is a data sample, 
+# and each dict have "image", "lidar", and "mask" keys
+# with the path to the corresponding file
+dataset = download_dataset(data_type, task, get_dataset=True)
+
+print(dataset)
