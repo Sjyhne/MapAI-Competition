28d46646c9462cef5e477d4b92479a3fae7c8cac
Pedro Alves <pmcalves1@gmail.com>
2022-11-11 14:14:13 +0100

    testing

---
+++ team_uiai/src/model.py
@@ -0,0 +1,39 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+# https://arxiv.org/abs/1610.00087
+class M5(nn.Module):
+    def __init__(self, n_output=35):
+        super().__init__()
+        self.conv1 = nn.Conv1d(1, 32, kernel_size=80, stride=16)
+        self.bn1 = nn.BatchNorm1d(32)
+        self.pool1 = nn.MaxPool1d(4)
+        self.conv2 = nn.Conv1d(32, 32, kernel_size=3)
+        self.bn2 = nn.BatchNorm1d(32)
+        self.pool2 = nn.MaxPool1d(4)
+        self.conv3 = nn.Conv1d(32, 2 * 32, kernel_size=3)
+        self.bn3 = nn.BatchNorm1d(2 * 32)
+        self.pool3 = nn.MaxPool1d(4)
+        self.conv4 = nn.Conv1d(2 * 32, 2 * 32, kernel_size=3)
+        self.bn4 = nn.BatchNorm1d(2 * 32)
+        self.pool4 = nn.MaxPool1d(4)
+        self.fc1 = nn.Linear(2 * 32, n_output)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(self.bn1(x))
+        x = self.pool1(x)
+        x = self.conv2(x)
+        x = F.relu(self.bn2(x))
+        x = self.pool2(x)
+        x = self.conv3(x)
+        x = F.relu(self.bn3(x))
+        x = self.pool3(x)
+        x = self.conv4(x)
+        x = F.relu(self.bn4(x))
+        x = self.pool4(x)
+        x = F.avg_pool1d(x, x.shape[-1])
+        x = x.permute(0, 2, 1)
+        x = self.fc1(x)
+        return F.log_softmax(x, dim=2)
