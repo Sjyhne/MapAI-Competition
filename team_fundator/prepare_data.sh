d631c27e6a919aa649c8d989628eeedae44134f3
LM239 <larsmartin@hodne.net>
2022-11-15 10:25:20 +0100

    add initial shell script

---
+++ team_fundator/prepare_data.sh
@@ -0,0 +1,14 @@
+cd src
+
+echo "Downloading dataset"
+python download.py
+
+cd data
+echo "Stitching dataset into original tiles"
+python combine_data.py
+
+echo "Splitting dataset with stride = 500/3"
+python split_data.py
+
+
+
