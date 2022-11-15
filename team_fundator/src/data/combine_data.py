0462a5e890660d4b6419f52c247d5178375d3905
LM239 <larsmartin@hodne.net>
2022-11-15 10:16:13 +0100

    fix slicing bug

--- team_fundator/src/data/combine_data.py
+++ team_fundator/src/data/combine_data.py
@@ -34,7 +34,8 @@ if __name__ == "__main__":
             
             # save the big tile
             new_stem = paths[0].stem[:-2] 
-            new_path = Path('./../../../data/big_tiles').joinpath("/".join(paths[0].parents[:2])).with_name(new_stem + ".tif")
+            new_parent = "/".join([paths[0].parents[0], paths[0].parents[1]])
+            new_path = Path('./../../../data/big_tiles').joinpath(new_parent).with_name(new_stem + ".tif")
             new_path.parent.mkdir(parents=True, exist_ok=True)
 
             cv2.imwrite(new_path.as_posix(), image)
