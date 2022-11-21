792f4ec5df8544d67e7998c0b3aa684db8dd50ae
LM239 <larsmartin@hodne.net>
2022-11-21 09:07:58 +0100

    add comments, remove unused files

--- team_fundator/src/bio_ensemble.py
+++ team_fundator/src/bio_ensemble.py
@@ -8,6 +8,7 @@ import cv2
 from utils import post_process_mask
 import random
 from competition_toolkit.eval_functions import iou, biou
+import argparse
 
 def normalize(arr):
     arr = np.clip(arr, 0.0, None)
@@ -15,26 +16,26 @@ def normalize(arr):
     return arr / s if s != 0 else arr 
 
 def prob(p):
+    #helper function which returns True with probability p
     return random.random() <= p
 
-def swap_shuffle(arr):
-    for i in range(len(arr) - 1, 0, -1):
-        j = random.randint(0, i - 1)
-        arr[[i, j]] = arr[[j, i]]
-    return arr
 
-class SegDataset(Dataset):
-
-    def __init__(self, ext=".tif"):
+class PredDataset(Dataset):
+    """
+    Dataset which loads predictions and tests the weightins of the ensemble.
+    Used to support multithreading
+    """
+    def __init__(self, args, ext=".tif"):
         
         self.weights = None
         print("Loading ensemble predictions")
-        self.pred = [np.load(name) for name in glob.glob( "data/ensemble_preds/task_2/*.npy")]
+        self.pred = [np.load(name) for name in glob.glob(f"data/ensemble_preds/task_{args.task}/*.npy")]
 
         print("Loading ground truths")
         self.masks = [cv2.imread(name, cv2.IMREAD_GRAYSCALE) for name in glob.glob("../../data/validation/masks/*" + ext)]
 
     def __len__(self):
+        # The number of different weights to test. Matches POP_SIZE in the EA
         return self.weights.shape[0]
 
     def __getitem__(self, idx):
@@ -44,6 +45,7 @@ class SegDataset(Dataset):
         weights = self.weights[idx]
         scores = np.zeros(len(self.pred))
 
+        # get the score for the weights at index idx
         for i, (pred, mask) in enumerate(zip(self.pred, self.masks)):
             pred = np.multiply(pred, weights)
             pred = np.sum(pred, axis=0)
@@ -55,6 +57,7 @@ class SegDataset(Dataset):
             ind_biou = biou(pred, mask)
             scores[i] = (ind_iou + ind_biou) / 2
 
+        # return mean score
         sample = {"score": np.mean(scores)}
 
         return sample
@@ -62,35 +65,34 @@ class SegDataset(Dataset):
     def set_weights(self, arr):
         self.weights = np.expand_dims(arr, (-1, -2))
 
-NUM_WORKERS=4
-POP_SIZE = 100
-IND_SIZE = 6
-GENS = 30
+POP_SIZE = 100 # 
+IND_SIZE = 6 # Number of weights to evolve / number of models in the ensemble
+GENS = 30 
 
-INIT_DROP_PROB = 0.1
-INIT_2X_PROB = 0.2
-MUTATION_PROB = 0.8
-CROSSOVER_FRAC = 0.8
-N_BEST = 1
+INIT_DROP_PROB = 0.1 # Probability of setting a weight to zero during initialisation of the population
+INIT_2X_PROB = 0.2 # probability of multiplying a weight with a small factor during initialisation of the population
+MUTATION_PROB = 0.85 # Probability of performaing a mutation (In crossover or otherwise)
+CROSSOVER_FRAC = 0.8 # Probability of performing crossover. Other wise mutation is (probably) done 
+N_BEST = 1 # Number of top fitness individuals in Gen N - 1 to replace the worst fit individuals in GEN N.
 
-BATCH_SIZE = 1
-
-if __name__ == "__main__":
-    pass
 
 def fitness(pop, dataloader):
     dataloader.dataset.set_weights(pop)
     fitnesses = np.zeros(pop.shape[0])
+    # Calculate fitness. One 'batch' is one individual
     for i, batch in tqdm(enumerate(dataloader), total=len(dataloader), desc="Calculating fitness"):
         fitnesses[i] = batch["score"]
     return fitnesses
 
 def bump_mutation(ind):
-    k = random.randint(0, IND_SIZE- 1)
+    # Mutate by multiplying with a small scalar at a random index
+    k = random.randint(0, IND_SIZE - 1)
     ind[k] *= 0.9 if prob(0.5) else 1.1
     return ind
 
 def noise_mutation(ind):
+    # With equal probability:
+    # Add (or subtract) some noise on the entire chromosome or a little more noise on one index
     if prob(0.5):
         ind += np.random.normal(scale=0.04, size=IND_SIZE)
         return ind
@@ -100,20 +102,24 @@ def noise_mutation(ind):
 
 
 def dist(ind1, ind2):
+    # L1 distance for crowding
     return np.linalg.norm(ind1 - ind2, ord=1)
 
 def crossover(p1, p2):
     c_index = random.randint(1, IND_SIZE - 2)
 
+    #crossover
     c1 = p1.copy()
     c1[c_index:] = p2[c_index:].copy()
 
     c2 = p2.copy()
     c2[c_index:] = p1[c_index:].copy()
     
+    #mutation
     c1 = mutate(c1)
     c2 = mutate(c2)
 
+    # crowd based on distance
     crowd_1_dist = dist(c1, p1) + dist(c2, p2)
     crowd_2_dist = dist(c1, p2) + dist(c2, p1)
     
@@ -124,19 +130,21 @@ def crossover(p1, p2):
         crowd_1 = np.array([p1, c2])    
         crowd_2 = np.array([p2, c1])
 
+    #return parent, child pais
     return crowd_1, crowd_2
 
 def uncrowd(crowd, p_fitnesses, dataloader):
+    # select individuals from parent, child paris based on best fit
     c_fit = fitness(crowd[1], dataloader)
     crowd_fit = np.array([p_fitnesses, c_fit])
     indeces = np.argmax(crowd_fit, axis=0)
     
-    return crowd[indeces, np.arange(POP_SIZE // 2)], crowd_fit[indeces, np.arange(POP_SIZE // 2)]
-
+    return crowd[indeces, np.arange(len(indeces))], crowd_fit[indeces, np.arange(len(indeces))]
 
 
 MUTATIONS = [bump_mutation, noise_mutation]
 def mutate(ind):
+    # With equal probability perform a mutation from MUTATIONS
     if not prob(MUTATION_PROB):
         return normalize(ind)
     for i, mutation in enumerate(MUTATIONS):
@@ -145,6 +153,7 @@ def mutate(ind):
         return normalize(mutation(ind))
 
 def init_pop():
+    # Initialise the population
     pop = np.zeros((POP_SIZE, IND_SIZE))
 
     for i in range(POP_SIZE):
@@ -161,8 +170,23 @@ def init_pop():
     return pop
 
 def main():
-    dataset = SegDataset()
-    dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=NUM_WORKERS)
+    global POP_SIZE
+    global IND_SIZE
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--task", type=int, default=1, help="Which task you are testing")
+    parser.add_argument("--size", type=int, default=4, help="How many ensembles are you testing?")
+    parser.add_argument("--workers", type=int, default=4, help="How many workers do you want?")
+    parser.add_argument("--pop-size", type=int, default=100, help="How large population do you want?")
+
+
+    args = parser.parse_args()
+
+    POP_SIZE = args.pop_size
+    IND_SIZE = args.size
+
+    dataset = PredDataset(args)
+    dataloader = DataLoader(dataset, batch_size=1, shuffle=False, num_workers=args.workers)
 
     pop = init_pop()
     fit = fitness(pop, dataloader)
@@ -174,11 +198,10 @@ def main():
         new_pop = np.zeros_like(pop)
         new_fit = np.zeros_like(fit)
         parent1_ids, parent2_ids = np.split(ids, 2)
-        #parent2_ids = swap_shuffle(parent1_ids.copy())
 
-        crowds_1 = np.zeros((2, POP_SIZE // 2, IND_SIZE))
-        crowds_2 = np.zeros((2, POP_SIZE // 2, IND_SIZE))
+        crowds = np.zeros((2, POP_SIZE, IND_SIZE))
 
+        # get next generation
         for i, (p1_id, p2_id) in enumerate(zip(parent1_ids, parent2_ids)):
             p1, p2 = pop[p1_id], pop[p2_id]
 
@@ -187,18 +210,19 @@ def main():
             else:
                 cr1, cr2 = np.array([p1, mutate(p1)]), np.array([p2, mutate(p2)])
 
-            crowds_1[:, i] = cr1
-            crowds_2[:, i] = cr2
+            crowds[:, i] = cr1
+            crowds[:, i + POP_SIZE // 2] = cr2
 
-        crowds_1, fit1 = uncrowd(np.array(crowds_1), fit[parent1_ids], dataloader)
-        crowds_2, fit2 = uncrowd(np.array(crowds_2), fit[parent2_ids], dataloader)
+        # finish crowding parent, child pairs
+        crowds, cr_fit = uncrowd(crowds, fit[ids], dataloader)
 
-        new_pop[parent1_ids] = crowds_1
-        new_pop[parent2_ids] = crowds_2
+        new_pop[parent1_ids] = crowds[:POP_SIZE // 2]
+        new_fit[parent1_ids] = cr_fit[:POP_SIZE // 2]
 
-        new_fit[parent1_ids] = fit1
-        new_fit[parent2_ids] = fit2
+        new_pop[parent2_ids] = crowds[POP_SIZE // 2:]
+        new_fit[parent2_ids] = cr_fit[POP_SIZE // 2:]
 
+        # replace the worst N individuals in this generation with the best N from the previous generation
         n_worst_new = np.argpartition(new_fit, N_BEST)[:N_BEST]
         n_best_old = np.argpartition(fit, -N_BEST)[-N_BEST:]
 
@@ -208,7 +232,7 @@ def main():
         fit = new_fit
         pop = new_pop
 
-        print("Current best fit: ", np.max(fit), pop[np.argmax(fit)])
+        print("Current best fit: ", np.max(fit), pop[np.argmax(fit)], "Median fit: ", np.median(fit))
     
     print(pop[n_worst_new])
     print(fit[n_worst_new])
