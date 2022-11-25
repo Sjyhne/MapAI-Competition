676bab3af8afa2e1efadff2ebdd4925f2e68df91
Lukasz Filip Mrozik <lfm@lawo-lab.no>
2022-11-25 18:35:43 +0000

    modified first layer for task 2

--- team_uiai/src/download.py
+++ team_uiai/src/download.py
@@ -1,6 +1,6 @@
 from competition_toolkit.dataloader import download_dataset
 data_type = "train" # or "validation"
-task = 1 # or 2, but does not matter for train and validation data types
+task = 2 # or 2, but does not matter for train and validation data types
 # paths is a list of dicts, where each dict is a data sample, 
 # and each dict have "image", "lidar", and "mask" keys
 # with the path to the corresponding file
