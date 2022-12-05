a657240a6dee8cd1bf66f74d131f0e478578e7c1
kab95 <kab95@hotmail.no>
2022-12-05 17:36:51 +0100

    initial commit

---
+++ team_kaborg/src/model_task_1.py
@@ -0,0 +1,147 @@
+import pathlib
+from tqdm import tqdm
+import torch
+import torchvision
+import numpy as np
+import cv2 as cv
+import yaml
+import matplotlib.pyplot as plt
+import gdown
+import os
+import shutil
+
+from competition_toolkit.dataloader import create_dataloader
+from competition_toolkit.eval_functions import iou, biou
+from ai_models.load_models import load_model
+from ai_models.create_models import load_resnet50, load_resnet101, load_unet, load_deepvision_resnet101
+
+def main(args):
+    #########################################################################
+    ###
+    # Load Model and its configuration
+    ###
+    #########################################################################
+    with open(args.config, "r") as f:
+        opts = yaml.load(f, Loader=yaml.Loader)
+        opts = {**opts, **vars(args)}
+
+    #########################################################################
+    ###
+    # Download Model Weights
+    # Use a mirror that is publicly available. This example uses Google Drive
+    ###
+    #########################################################################
+    
+    #pt_share_link = "https://drive.google.com/file/d/1wv5H_MexHhlfcxJfOUWB3Eo51ciuGJ7y/view?usp=sharing"
+    #pt_share_link = "https://drive.google.com/file/d/18m8A3wjzhE8rbaxHCByA3LVn1eCreV-8/view?usp=share_link"
+    #pt_share_link = "https://drive.google.com/file/d/1C3zu4nvsbAy8Q-CqFXQF8prstFCxVYw5/view?usp=sharing"
+    #pt_share_link = "https://drive.google.com/file/d/1MHpOCO7gzseOWBS2B6pPGbu6MwLEAYk6/view?usp=share_link"
+    #pt_share_link = "https://drive.google.com/file/d/1yveA77yPVzZ5OlL89K9ZyK9nw5byZhYE/view?usp=share_link"
+    pt_share_link = "https://drive.google.com/file/d/15WP4OrMOQ8Ik5MZgMRlASdqISeeQpjnO/view?usp=sharing"
+    pt_id = pt_share_link.split("/")[-2]
+    # Download trained model ready for inference
+    url_to_drive = f"https://drive.google.com/uc?id={pt_id}"
+    model_checkpoint = "pretrained_task1.pt"
+
+    gdown.download(url_to_drive, model_checkpoint, quiet=False)
+
+    #########################################################################
+    ###
+    # Create needed directories for data
+    ###
+    #########################################################################
+    task_path = pathlib.Path(args.submission_path).joinpath(f"task_{opts['task']}")
+    opts_file = task_path.joinpath("opts.yaml")
+    predictions_path = task_path.joinpath("predictions")
+    if task_path.exists():
+        shutil.rmtree(task_path.absolute())
+    predictions_path.mkdir(exist_ok=True, parents=True)
+
+    #########################################################################
+    ###
+    # Setup Model
+    ###
+    #########################################################################
+    device = opts["device"]
+    #model, get_output = load_model(load_deepvision_resnet101, model_checkpoint, opts)
+    model, get_output = load_model(load_unet, model_checkpoint, opts)
+    #########################################################################
+    ###
+    # Load Data
+    ###
+    #########################################################################
+
+    dataloader = create_dataloader(opts, opts["data_type"])
+    print(dataloader)
+
+    iou_scores = np.zeros((len(dataloader)))
+    biou_scores = np.zeros((len(dataloader)))
+    for idx, (image, label, filename) in tqdm(enumerate(dataloader), total=len(dataloader), desc="Inference",
+                                              leave=False):
+
+        # Split filename and extension
+        filename_base, file_extension = os.path.splitext(filename[0])
+
+        # Send image and label to device (eg., cuda)
+        image = image.to(device)
+        label = label.to(device)
+
+        # Perform model prediction
+        prediction = model(image)[get_output]
+        if opts["device"] == "cpu":
+            prediction = torch.argmax(torch.softmax(prediction, dim=1), dim=1).squeeze().detach().numpy()
+        else:
+            prediction = torch.argmax(torch.softmax(prediction, dim=1), dim=1).squeeze().cpu().detach().numpy()
+        # Postprocess prediction
+
+        if opts["device"] == "cpu":
+            label = label.squeeze().detach().numpy()
+        else:
+            label = label.squeeze().cpu().detach().numpy()
+
+        prediction = np.uint8(prediction)
+        label = np.uint8(label)
+        assert prediction.shape == label.shape, f"Prediction and label shape is not same, pls fix [{prediction.shape} - {label.shape}]"
+
+        # Predict score
+        iou_score = iou(prediction, label)
+        biou_score = biou(label, prediction)
+
+        iou_scores[idx] = np.round(iou_score, 6)
+        biou_scores[idx] = np.round(biou_score, 6)
+
+        prediction_visual = np.copy(prediction)
+
+        for idx, value in enumerate(opts["classes"]):
+            prediction_visual[prediction_visual == idx] = opts["class_to_color"][value]
+
+        if opts["device"] == "cpu":
+            image = image.squeeze().detach().numpy()[:3, :, :].transpose(1, 2, 0)
+        else:
+            image = image.squeeze().cpu().detach().numpy()[:3, :, :].transpose(1, 2, 0)
+
+        fig, ax = plt.subplots(1, 3)
+        columns = 3
+        rows = 1
+        ax[0].set_title("Input (RGB)")
+        ax[0].imshow(image)
+        ax[1].set_title("Prediction")
+        ax[1].imshow(prediction_visual)
+        ax[2].set_title("Label")
+        ax[2].imshow(label)
+
+        # Save to file.
+        predicted_sample_path_png = predictions_path.joinpath(f"{filename_base}.png")
+        predicted_sample_path_tif = predictions_path.joinpath(filename[0])
+        plt.savefig(str(predicted_sample_path_png))
+        plt.close()
+        cv.imwrite(str(predicted_sample_path_tif), prediction)
+        image = cv.imread(str(predicted_sample_path_tif), 1)
+        reszise = cv.resize(image, (500, 500))
+        cv.imwrite(str(predicted_sample_path_tif), reszise)
+   
+
+    print("iou_score:", np.round(iou_scores.mean(), 5), "biou_score:", np.round(biou_scores.mean(), 5))
+
+    # Dump file configuration
+    yaml.dump(opts, open(opts_file, "w"), Dumper=yaml.Dumper)
---
+++ team_kaborg/src/visualisation.py
@@ -0,0 +1,81 @@
+# visulize the output of the model
+
+from black import out
+import matplotlib.pyplot as plt
+import numpy as np
+from augmentation.dataloader import create_dataloader
+# from competition_toolkit.dataloader import create_dataloader
+from ai_models.load_models import load_model
+from ai_models.create_models import load_resnet50, load_resnet101, load_unet
+from ai_models.utils import get_opts
+
+
+def visualize_output(model, image, label, get_output):
+    model.eval()
+
+    output = model(image.cuda())[get_output]
+
+    output = output.cpu().detach().numpy()
+    image = image.cpu().detach().numpy()
+    label = label.cpu().detach().numpy()
+
+    fig, ax = plt.subplots(1, 3, figsize=(15, 15))
+    image = np.transpose(image[0], (1, 2, 0))
+    label = np.transpose(label, (1, 2, 0))
+    output = output.astype(np.uint8)
+    output = np.transpose(output[0], (1, 2, 0))
+    output = 1 - output.argmax(axis=2)
+
+    ax[0].imshow(image)
+    ax[0].set_title("Image")
+    ax[1].imshow(label)
+    ax[1].set_title("Label")
+    ax[2].imshow(output)
+    ax[2].set_title("Output")
+    plt.show()
+
+# This visualizes the model training and test output
+def visulize_training(path_training_folder):
+    # get run log file
+    # {'epoch': 0, 'trainloss': 0.1063, 'testloss': 0.0795, 'trainiou': 0.5633, 'testiou': 0.5732, 'trainbiou': 0.4875, 'testbiou': 0.5, 'trainscore': 0.5254, 'testscore': 0.5366}
+    # get trainscore and testscore
+    # plot trainscore and testscore
+
+    train_scores = []
+    test_scores = []
+    with open(path_training_folder + "/run.log", "r") as f:
+        for line in f.readlines():
+            line = line.strip()
+            if line:
+                line = line.split(" ")
+                train_scores.append(float(line[15].replace(",", "")))
+                test_scores.append(float(line[17].replace(",", "").replace("}", "")))
+    
+    plt.plot(train_scores, label="train")
+    plt.plot(test_scores, label="test")
+    plt.legend()
+    plt.savefig(path_training_folder + "/train_test_score.png")
+    plt.show()
+    # save
+    plt.close()
+    
+
+    
+
+if __name__ == "__main__":
+    opts = get_opts()
+    model_path = "backup\\unet_lr_loss_batches\\best_task1_7.pt"
+    model, get_output = load_model(load_unet, model_path, opts)
+    # model_path = "backup\\resnet50_scratch\\best_task1_4.pt"
+    # model, get_output = load_model(load_resnet50, model_path, opts)
+    dataloader = create_dataloader(opts, "train")
+    for idx, batch in enumerate(dataloader):
+        image, label, filename = batch
+        visualize_output(model, image, label, get_output)
+
+    run_log_path = "backup\\unet_with_augmentation"
+    model_path = "pretrained_task2.pt"
+    visulize_training(run_log_path)
+
+            
+    
