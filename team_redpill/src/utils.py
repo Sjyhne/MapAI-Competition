8c6a3316d3978df1a0716b28e4b9439e1aa13103
jediofgever <fetulahatas1@gmail.com>
2022-12-05 23:59:26 +0100

    add execution instructions

--- team_redpill/src/utils.py
+++ team_redpill/src/utils.py
@@ -1,6 +1,9 @@
 import os
 import glob
 import torch
+import numpy as np
+from torch.utils.data import Dataset
+from PIL import Image
 
 def create_run_dir(opts):
 
@@ -43,3 +46,315 @@ def record_scores(opts, scoredict):
 
     with open(os.path.join(rundir, "run.log"), "a") as f:
         f.write(str(scoredict) + "\n")
+
+
+class BuildingDataset(Dataset):
+    def __init__(self, img_paths, mask_paths, size=(256, 256), transform=None):
+        self.img_paths = img_paths
+        self.mask_paths = mask_paths
+        self.size = size
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.img_paths)
+
+    def __getitem__(self, idx):
+        image = np.array(Image.open(self.img_paths[idx]).convert("RGB"))
+        mask = np.array(Image.open(self.mask_paths[idx]))
+        mask[mask > 0.0] = 1.0
+        sample = dict(image=image, mask=mask)
+        if self.transform is not None:
+            sample = self.transform(**sample)
+
+        return sample
+
+
+class SimpleBuildingDataset(BuildingDataset):
+    def __getitem__(self, *args, **kwargs):
+        sample = super().__getitem__(*args, **kwargs)
+        # resize images
+        image = np.array(Image.fromarray(sample["image"]).resize((256, 256), Image.LINEAR))
+        mask = np.array(Image.fromarray(sample["mask"]).resize((256, 256), Image.NEAREST))
+        # convert to other format HWC -> CHW
+        sample["image"] = np.moveaxis(image, -1, 0)
+        sample["mask"] = np.expand_dims(mask, 0)
+        return sample        
+
+class BuildingLidarDataset(Dataset):
+    def __init__(self, lidar_paths, mask_paths, size=(256, 256), transform=None):
+        self.lidar_paths = lidar_paths
+        self.mask_paths = mask_paths
+        self.size = size
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.lidar_paths)
+
+    def __getitem__(self, idx):
+        lidar = np.array(Image.open(self.lidar_paths[idx]), dtype=np.float32)
+        mask = np.array(Image.open(self.mask_paths[idx]))
+        mask[mask > 0.0] = 1.0
+        
+        sample = dict(lidar=lidar, mask=mask)
+        if self.transform is not None:
+            sample = self.transform(**sample)
+
+        return sample
+
+
+class SimpleBuildingLidarDataset(BuildingLidarDataset):
+    def __getitem__(self, *args, **kwargs):
+        sample = super().__getitem__(*args, **kwargs)
+        # resize images
+        lidar = np.array(Image.fromarray(sample["lidar"]).resize((256, 256), Image.LINEAR))
+        mask = np.array(Image.fromarray(sample["mask"]).resize((256, 256), Image.NEAREST))
+        # convert to other format HWC -> CHW
+        sample["lidar"] = np.expand_dims(lidar, 0)
+        sample["mask"] = np.expand_dims(mask, 0)
+        return sample
+
+
+import pytorch_lightning as pl
+import segmentation_models_pytorch as smp
+
+class PetModel(pl.LightningModule):
+
+    def __init__(self, arch, encoder_name, in_channels, out_classes, **kwargs):
+        super().__init__()
+        self.model = smp.create_model(
+            arch, encoder_name=encoder_name, in_channels=in_channels, classes=out_classes, **kwargs
+        )
+
+        # preprocessing parameteres for image
+        params = smp.encoders.get_preprocessing_params(encoder_name)
+
+        self.register_buffer("std", torch.tensor(params["std"]).view(1, 3, 1, 1))
+        self.register_buffer("mean", torch.tensor(params["mean"]).view(1, 3, 1, 1))
+
+        # for image segmentation dice loss could be the best first choice
+        self.loss_fn = smp.losses.DiceLoss(smp.losses.BINARY_MODE, from_logits=True)
+
+    def forward(self, image):
+        # normalize image here
+        image = (image - self.mean) / self.std
+        mask = self.model(image)
+        return mask
+
+    def shared_step(self, batch, stage):
+        
+        image = batch["image"]
+
+        # Shape of the image should be (batch_size, num_channels, height, width)
+        # if you work with grayscale images, expand channels dim to have [batch_size, 1, height, width]
+        assert image.ndim == 4
+
+        # Check that image dimensions are divisible by 32, 
+        # encoder and decoder connected by `skip connections` and usually encoder have 5 stages of 
+        # downsampling by factor 2 (2 ^ 5 = 32); e.g. if we have image with shape 65x65 we will have 
+        # following shapes of features in encoder and decoder: 84, 42, 21, 10, 5 -> 5, 10, 20, 40, 80
+        # and we will get an error trying to concat these features
+        h, w = image.shape[2:]
+        assert h % 32 == 0 and w % 32 == 0
+
+        mask = batch["mask"]
+
+        # Shape of the mask should be [batch_size, num_classes, height, width]
+        # for binary segmentation num_classes = 1
+        assert mask.ndim == 4
+
+        # Check that mask values in between 0 and 1, NOT 0 and 255 for binary segmentation
+        assert mask.max() <= 1.0 and mask.min() >= 0
+
+        logits_mask = self.forward(image)
+        
+        # Predicted mask contains logits, and loss_fn param `from_logits` is set to True
+        loss = self.loss_fn(logits_mask, mask)
+
+        # Lets compute metrics for some threshold
+        # first convert mask values to probabilities, then 
+        # apply thresholding
+        prob_mask = logits_mask.sigmoid()
+        pred_mask = (prob_mask > 0.5).float()
+
+        # We will compute IoU metric by two ways
+        #   1. dataset-wise
+        #   2. image-wise
+        # but for now we just compute true positive, false positive, false negative and
+        # true negative 'pixels' for each image and class
+        # these values will be aggregated in the end of an epoch
+        tp, fp, fn, tn = smp.metrics.get_stats(pred_mask.long(), mask.long(), mode="binary")
+
+        return {
+            "loss": loss,
+            "tp": tp,
+            "fp": fp,
+            "fn": fn,
+            "tn": tn,
+        }
+
+    def shared_epoch_end(self, outputs, stage):
+        # aggregate step metics
+        tp = torch.cat([x["tp"] for x in outputs])
+        fp = torch.cat([x["fp"] for x in outputs])
+        fn = torch.cat([x["fn"] for x in outputs])
+        tn = torch.cat([x["tn"] for x in outputs])
+
+        # per image IoU means that we first calculate IoU score for each image 
+        # and then compute mean over these scores
+        per_image_iou = smp.metrics.iou_score(tp, fp, fn, tn, reduction="micro-imagewise")
+        
+        # dataset IoU means that we aggregate intersection and union over whole dataset
+        # and then compute IoU score. The difference between dataset_iou and per_image_iou scores
+        # in this particular case will not be much, however for dataset 
+        # with "empty" images (images without target class) a large gap could be observed. 
+        # Empty images influence a lot on per_image_iou and much less on dataset_iou.
+        dataset_iou = smp.metrics.iou_score(tp, fp, fn, tn, reduction="micro")
+
+        metrics = {
+            f"{stage}_per_image_iou": per_image_iou,
+            f"{stage}_dataset_iou": dataset_iou,
+        }
+        
+        self.log_dict(metrics, prog_bar=True)
+
+    def training_step(self, batch, batch_idx):
+        return self.shared_step(batch, "train")            
+
+    def training_epoch_end(self, outputs):
+        return self.shared_epoch_end(outputs, "train")
+
+    def validation_step(self, batch, batch_idx):
+        return self.shared_step(batch, "valid")
+
+    def validation_epoch_end(self, outputs):
+        return self.shared_epoch_end(outputs, "valid")
+
+    def test_step(self, batch, batch_idx):
+        return self.shared_step(batch, "test")  
+
+    def test_epoch_end(self, outputs):
+        return self.shared_epoch_end(outputs, "test")
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=0.0001)
+
+class LidarNet(pl.LightningModule):
+
+    def __init__(self, arch, encoder_name, in_channels, out_classes, **kwargs):
+        super().__init__()
+        self.model = smp.create_model(
+            arch, encoder_name=encoder_name, in_channels=in_channels, classes=out_classes, **kwargs
+        )
+
+        # preprocessing parameteres for image
+        #params = smp.encoders.get_preprocessing_params(encoder_name)
+        #self.register_buffer("std", torch.tensor(params["std"]).view(1, 3, 1, 1))
+        #self.register_buffer("mean", torch.tensor(params["mean"]).view(1, 3, 1, 1))
+
+        # for image segmentation dice loss could be the best first choice
+        self.loss_fn = smp.losses.DiceLoss(smp.losses.BINARY_MODE, from_logits=True)
+
+    def forward(self, lidar):
+        # normalize image here
+        #image = (image - self.mean) / self.std
+        mask = self.model(lidar)
+        return mask
+
+    def shared_step(self, batch, stage):
+        
+        lidar = batch["lidar"]
+
+        # Shape of the image should be (batch_size, num_channels, height, width)
+        # if you work with grayscale images, expand channels dim to have [batch_size, 1, height, width]
+        assert lidar.ndim == 4
+
+        # Check that image dimensions are divisible by 32, 
+        # encoder and decoder connected by `skip connections` and usually encoder have 5 stages of 
+        # downsampling by factor 2 (2 ^ 5 = 32); e.g. if we have image with shape 65x65 we will have 
+        # following shapes of features in encoder and decoder: 84, 42, 21, 10, 5 -> 5, 10, 20, 40, 80
+        # and we will get an error trying to concat these features
+        h, w = lidar.shape[2:]
+        assert h % 32 == 0 and w % 32 == 0
+
+        mask = batch["mask"]
+
+        # Shape of the mask should be [batch_size, num_classes, height, width]
+        # for binary segmentation num_classes = 1
+        assert mask.ndim == 4
+
+        # Check that mask values in between 0 and 1, NOT 0 and 255 for binary segmentation
+        assert mask.max() <= 1.0 and mask.min() >= 0
+
+        logits_mask = self.forward(lidar)
+        
+        # Predicted mask contains logits, and loss_fn param `from_logits` is set to True
+        loss = self.loss_fn(logits_mask, mask)
+
+        # Lets compute metrics for some threshold
+        # first convert mask values to probabilities, then 
+        # apply thresholding
+        prob_mask = logits_mask.sigmoid()
+        pred_mask = (prob_mask > 0.5).float()
+
+        # We will compute IoU metric by two ways
+        #   1. dataset-wise
+        #   2. image-wise
+        # but for now we just compute true positive, false positive, false negative and
+        # true negative 'pixels' for each image and class
+        # these values will be aggregated in the end of an epoch
+        tp, fp, fn, tn = smp.metrics.get_stats(pred_mask.long(), mask.long(), mode="binary")
+
+        return {
+            "loss": loss,
+            "tp": tp,
+            "fp": fp,
+            "fn": fn,
+            "tn": tn,
+        }
+
+    def shared_epoch_end(self, outputs, stage):
+        # aggregate step metics
+        tp = torch.cat([x["tp"] for x in outputs])
+        fp = torch.cat([x["fp"] for x in outputs])
+        fn = torch.cat([x["fn"] for x in outputs])
+        tn = torch.cat([x["tn"] for x in outputs])
+
+        # per image IoU means that we first calculate IoU score for each image 
+        # and then compute mean over these scores
+        per_image_iou = smp.metrics.iou_score(tp, fp, fn, tn, reduction="micro-imagewise")
+        
+        # dataset IoU means that we aggregate intersection and union over whole dataset
+        # and then compute IoU score. The difference between dataset_iou and per_image_iou scores
+        # in this particular case will not be much, however for dataset 
+        # with "empty" images (images without target class) a large gap could be observed. 
+        # Empty images influence a lot on per_image_iou and much less on dataset_iou.
+        dataset_iou = smp.metrics.iou_score(tp, fp, fn, tn, reduction="micro")
+
+        metrics = {
+            f"{stage}_per_image_iou": per_image_iou,
+            f"{stage}_dataset_iou": dataset_iou,
+        }
+        
+        self.log_dict(metrics, prog_bar=True)
+
+    def training_step(self, batch, batch_idx):
+        return self.shared_step(batch, "train")            
+
+    def training_epoch_end(self, outputs):
+        return self.shared_epoch_end(outputs, "train")
+
+    def validation_step(self, batch, batch_idx):
+        return self.shared_step(batch, "valid")
+
+    def validation_epoch_end(self, outputs):
+        return self.shared_epoch_end(outputs, "valid")
+
+    def test_step(self, batch, batch_idx):
+        return self.shared_step(batch, "test")  
+
+    def test_epoch_end(self, outputs):
+        return self.shared_epoch_end(outputs, "test")
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=0.0001)
+
